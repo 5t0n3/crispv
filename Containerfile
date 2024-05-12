d1ab927a3d3d5c38e6a545b58ac68e6d7f51afaf
Zane <git@formulaic.cloud>
2024-05-12 15:46:13 -0700

    initial web server & csv parsing code, no html rendering yet

--- Containerfile
+++ Containerfile
@@ -2,13 +2,14 @@ FROM haskell:slim
 
 RUN cabal update
 
+# dependency caching yay
 WORKDIR /opt/crispv
-
 COPY crispv.cabal .
-
 RUN cabal build --only-dependencies -j4
 
+# actually build crispv
 COPY . .
 RUN cabal install
 
+# and run it :)
 CMD ["crispv"]
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,22 @@
 module Main where
 
+import Control.Monad
+import Data.ByteString.Builder (stringUtf8, toLazyByteString)
+import Data.Csv
+import Data.Text (Text)
+import Data.Vector as V
+import Happstack.Server (ServerPart, dir, look, nullConf, ok, simpleHTTP, toResponse)
+
+csvPart :: ServerPart String
+csvPart = do
+  csvStr <- look "csvdata"
+  let csvBytes = toLazyByteString $ stringUtf8 csvStr
+      -- TODO: header included? (for table)
+      parsed = decode NoHeader csvBytes :: Either String (V.Vector Record)
+  case parsed of
+    Left _ -> mzero
+    Right stuff -> ok $ show stuff
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main =
+  simpleHTTP nullConf $ dir "render" csvPart
