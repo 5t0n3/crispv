f854e051cd99f08dd0e1c3228ff59a630c50c3c1
Zane <git@formulaic.cloud>
2024-05-12 17:12:31 -0700

    clean up stuff a bit & comment

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
 import Control.Monad
@@ -8,39 +6,47 @@ import Data.Csv
 import Data.Text (Text)
 import Data.Vector (Vector)
 import qualified Data.Vector as V
-import Happstack.Server (Response, ServerPart, dir, look, nullConf, ok, simpleHTTP, toResponse)
+import Happstack.Server (Response, ServerPart, badRequest, dir, look, notFound, nullConf, ok, simpleHTTP, toResponse)
 import Text.Blaze ((!))
 import qualified Text.Blaze.Html5 as H
 
 type TextRecord = Vector Text
 
+-- Reducing function for consistentLength
 widthReducer :: (Bool, Int) -> TextRecord -> (Bool, Int)
-widthReducer (allSame, prevLength) record = (allSame && consistent, len)
+widthReducer (consistent, prevLength) record = (consistent && len == prevLength, len)
   where
     len = V.length record
-    consistent = len == prevLength
 
-getTableWidth :: Vector TextRecord -> Either String Int
-getTableWidth csvRecords = if allSameLength then Right length else Left "not all same length"
+-- Ensures all CSV records in the given record are of the same length
+consistentLength :: Vector TextRecord -> Bool
+consistentLength csvRecords = allSameLength
   where
     firstRec = (V.!?) csvRecords 0
     initialAcc = case firstRec of
       Just vec -> (True, V.length vec)
+      -- if there aren't any records, treat that as a bad request
       Nothing -> (False, 0)
     (allSameLength, length) = V.foldl' widthReducer initialAcc csvRecords
 
--- Converts a record into a row (<tr>), where each field is placed in a <td> element
+-- Converts a record into a <tr>, where each field is wrapped with a <td> element
 recordToRow :: TextRecord -> H.Html
 recordToRow = H.tr . mapM_ (H.td . H.toHtml)
 
+-- Renders CSV data into a table, ensuring it is nonempty & each row has the same length
 csvToTable :: Vector TextRecord -> Either String H.Html
-csvToTable records = do
-  width <- getTableWidth records
-  return $ H.table $ forM_ records recordToRow
+csvToTable records
+  | consistentLength records = Right renderedTable
+  | otherwise = Left "bad records"
+  where
+    -- TODO: header (index + V.slice?)
+    renderedTable = H.table $ forM_ records recordToRow
 
+-- Handler for /render route, which does the HTML rendering of CSV
 csvPart :: ServerPart Response
 csvPart = do
   csvStr <- look "csvdata"
+  -- decode takes a ByteString so we have to do the conversion manually
   let csvBytes = toLazyByteString $ stringUtf8 csvStr
       -- TODO: header included? (for table)
       parsed = decode NoHeader csvBytes
@@ -51,4 +57,11 @@ csvPart = do
 
 main :: IO ()
 main =
-  simpleHTTP nullConf $ dir "render" csvPart
+  simpleHTTP nullConf $
+    msum
+      [ dir "render" csvPart,
+        -- if previous route failed, then something went wrong when rendering the CSV
+        dir "render" $ badRequest $ toResponse "invalid CSV",
+        -- default to not found for non-render routes
+        notFound $ toResponse "not found"
+      ]
